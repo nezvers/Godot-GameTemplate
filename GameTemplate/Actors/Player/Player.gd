f96b709ecffcb9c2117e5c7be7248a24c02ed7ec
Eric Catlin <eroccatlun@gmail.com>
2021-01-04 22:21:33 -0800

    Player script is slightly modified timednode which integrates during get_frame

--- GameTemplate/Actors/Player/Player.gd
+++ GameTemplate/Actors/Player/Player.gd
@@ -1,21 +1,64 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-enum STATE { PLAYING, 
-REPLAYING, 
-PAUSED }
-var velocity = Vector2()
-var timelien_state;
-# Declare member variables here. Examples:
-
-
-
-# Called when the node enters the scene tree for the first time.
+onready var timeline_interface = $TimelineInterface
 func _ready():
-	pass # Replace with function body.
+	timeline_interface.connect("get_frame", self, "get_frame")
+	timeline_interface.connect("apply_frame", self, "apply_frame")
+	timeline_interface.connect("playing_changed", self, "playing_changed")
+		 
+func get_frame():
+	if not Engine.is_in_physics_frame():
+		yield(get_tree(), "physics_frame")
+	var physics_state = Physics2DServer.body_get_direct_state(get_rid())
+	
+	var transform = physics_state.transform
+	var linear_velocity = physics_state.linear_velocity
+	var angular_velocity = physics_state.angular_velocity
+	
+	var dir = Vector2.ZERO
+	dir.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	dir.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 
-func _timeline_state_update(state):
+	linear_velocity += dir*10;
+
+	timeline_interface.done({
+		"transform": transform,
+		"linear_velocity": linear_velocity,
+		"angular_velocity": angular_velocity
+	})
+
+	global_transform = transform
+	physics_state.transform = transform
+	physics_state.linear_velocity = linear_velocity
+	physics_state.angular_velocity = angular_velocity
+
+	physics_state.integrate_forces()
 
 
-func _physics_process(delta):
-	if timeline.state === STATE.PLAYING: 
-	move_and_slide(velocity, Vector2.UP, false, 4, deg2rad(45), true)
+func apply_frame(frame, prev_frame, delta):
+	if not Engine.is_in_physics_frame():
+		yield(get_tree(), "physics_frame")
+	var physics_state = Physics2DServer.body_get_direct_state(get_rid())
+	
+	var transform = prev_frame.transform.interpolate_with(frame.transform, delta)
+	var linear_velocity = prev_frame.linear_velocity.linear_interpolate(frame.linear_velocity, delta)
+	var angular_velocity = lerp(prev_frame.angular_velocity, frame.angular_velocity, delta);
+	
+	global_transform = transform
+	physics_state.transform = transform
+	physics_state.linear_velocity = linear_velocity
+	physics_state.angular_velocity = angular_velocity
+
+	physics_state.integrate_forces()
+
+
+func playing_changed(playing):
+	var PLAYING = timeline_interface.PLAYING
+	if playing == PLAYING.PAUSED:
+		call_deferred("set_mode", RigidBody2D.MODE_STATIC)
+	elif playing == PLAYING.PLAYING:
+		call_deferred("set_mode", RigidBody2D.MODE_RIGID)
+
+
+
+
