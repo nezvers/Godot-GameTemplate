912ff389d7213248a7cb832417af58e6a4d45742
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-08-25 22:30:14 -0300

    Delete comments

--- addons/GameTemplate/Autoload/GlobalTimer.gd
+++ addons/GameTemplate/Autoload/GlobalTimer.gd
@@ -1,15 +1,5 @@
 extends Node2D
 
-# Usage from outside:
-#
-# 		func _on_Button_pressed()->void:
-# 			GlobalTimer.add_timeout(self, "hello_wordl", 3)
-#
-# 		func hello_wordl():
-# 			print("Hello, World!")
-#
-# TODO: delete_timeout()
-
 func add_timeout(node : Node, function : String, timeout : int)->void:
 	var timer = Timer.new()
 	timer = Timer.new()
