4c84a7c0810550957595e2cca1931d3b37bf867f
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-09-11 00:57:56 +0300

    stats as files, projectile spawner

---
+++ scripts/weapon_system/projectile/ProjectileSpawner.gd
@@ -0,0 +1,47 @@
+## Handles spawning new projectiles
+class_name ProjectileSpawner
+extends Node
+
+signal projectile_created
+## signal before spawning for components prepare angle array
+signal prepare_spawn
+
+## If enabled is false then spawning will be cancelled
+@export var enabled:bool = true
+## global_position spawning point from which actual position will be calculated
+@export var projectile_position:Vector2
+## Scene that will be instantiated to create a projectile
+## Direction a projectile will fly
+@export var direction:Vector2
+## offset distance in the direction
+@export var initial_distance:float
+## Scene from which a new projectile will be created
+@export var projectile_scene:PackedScene
+## Will be set for a new projectile
+@export_flags_2d_physics var collision_mask:int
+## Movement modifier to fake angled perspective
+@export var axis_multiplication:Vector2 = Vector2(1.0, 0.5)
+## Reference to projectile parent
+@export var projectile_parent_reference:ReferenceNodeResource
+## Angle offsets for each projectiles
+## No angle, no projectile
+## Use prepare_spawn signal to manipulate spread
+@export var projectile_angles:Array[float] = [0.0]
+## Used for defining projectile damage
+@export var projectile_damage:int = 1
+
+func spawn()->void:
+	if !enabled:
+		return
+	assert(projectile_scene != null, "no projectile scene assigned")
+	assert(projectile_parent_reference.node != null, "projectile parent reference isn't set")
+	
+	prepare_spawn.emit()
+	for angle:float in projectile_angles:
+		var inst:Projectile2D = projectile_scene.instantiate()
+		inst.direction = direction.normalized()
+		inst.damage = projectile_damage
+		inst.collision_mask = Bitwise.append_flags(inst.collision_mask, collision_mask)
+		inst.global_position = initial_distance * direction * axis_multiplication + projectile_position
+		projectile_parent_reference.node.add_child(inst)
+	projectile_created.emit()
---
+++ scripts/weapon_system/projectile/SubProjectileManager.gd
@@ -0,0 +1,27 @@
+class_name SubProjectileManager
+extends Node
+
+@export var projectile:Projectile2D
+@export var projectile_spawner:ProjectileSpawner
+## Scene created and the ready()
+@export var start_projectile_scene:PackedScene
+@export var exit_projectile_scene:PackedScene
+
+func _ready()->void:
+	# TODO: just a mockup. Need some kind per situation configurations
+	projectile_spawner.collision_mask = Bitwise.append_flags(projectile_spawner.collision_mask, projectile.collision_mask)
+	projectile_spawner.projectile_damage = projectile.damage
+	projectile_spawner.axis_multiplication = projectile.axis_multiplier
+	
+	if start_projectile_scene != null:
+		spawn(start_projectile_scene)
+	
+	if exit_projectile_scene != null:
+		projectile.tree_exiting.connect(spawn.bind(start_projectile_scene))
+
+## Sets up projectile spawner and call spawns
+func spawn(scene:PackedScene)->void:
+	projectile_spawner.projectile_scene = scene
+	projectile_spawner.projectile_position = projectile.global_position
+	projectile_spawner.direction = projectile.direction
+	projectile_spawner.spawn()
