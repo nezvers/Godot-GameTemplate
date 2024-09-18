305ed680890385af6da9d10642234472c92aff06
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-09-18 22:58:36 +0300

    Camera shake & fix: DamageResource report

--- scripts/weapon_system/WeaponManager.gd
+++ scripts/weapon_system/WeaponManager.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 ## Emitted when weapon is changed
 signal weapon_changed
+signal damage_report(damage:DamageResource)
 
 ## Relative to this manager
 ## Will be modified to correct for instanced wepons
@@ -14,6 +15,9 @@ signal weapon_changed
 ## Passed to instantiated weapons, that pass it to projectiles
 ## TODO: Need to remove passing chain
 @export_flags_2d_physics var collision_mask:int
+## In case users share the same weapons it's better to make damage_resource unique
+## That removes opion to tweak values from Godot Editor while a game is running
+@export var make_unique_damage:bool = true
 
 ## List of instanced weapons available to the user
 ## Weapons that are already in the scene tree are added on _ready
@@ -23,15 +27,28 @@ var weapon_index:int = 0
 ## Currently activated weapon
 var current_weapon:Weapon = null
 
-
-func _ready()->void:
-	mover.input_resource.switch_weapon.connect(on_switch_weapon)
+func _init()->void:
 	for weapon:Node in get_children():
 		if !(weapon is Weapon):
 			continue
-		# every weapon are disabled by default
-		weapon.set_enabled(false)
-		weapon_list.append(weapon)
+		if make_unique_damage:
+			weapon.damage_resource = weapon.damage_resource.duplicate()
+			weapon.damage_resource.resource_name += "dup_"
+		if !weapon.damage_resource.damage_report.is_connected(on_damage_report):
+			weapon.damage_resource.damage_report.connect(on_damage_report)
+
+func _ready()->void:
+	mover.input_resource.switch_weapon.connect(on_switch_weapon)
+	
+	
+	for weapon:Node in get_children():
+		if !(weapon is Weapon):
+			continue
+		var damage_resource_dup:DamageResource = weapon.damage_resource.duplicate()
+		var packed_scene:PackedScene = ScenePacker.create_package(weapon)
+		auto_instance_weapons.append(packed_scene)
+		remove_child(weapon)
+		weapon.queue_free()
 	
 	for scene:PackedScene in auto_instance_weapons:
 		add_new_weapon_from_scene(scene)
@@ -45,6 +62,10 @@ func add_new_weapon_from_scene(scene:PackedScene)->void:
 	weapon.enabled = false
 	weapon.mover = mover
 	weapon.collision_mask = collision_mask
+	if make_unique_damage:
+		weapon.damage_resource = weapon.damage_resource.duplicate()
+	if !weapon.damage_resource.damage_report.is_connected(on_damage_report):
+		weapon.damage_resource.damage_report.connect(on_damage_report)
 	add_child(weapon)
 	weapon_list.append(weapon)
 
@@ -69,3 +90,10 @@ func set_weapon_index(value:int)->void:
 	current_weapon = weapon_list[weapon_index]
 	current_weapon.set_enabled(true)
 	weapon_changed.emit()
+
+func get_current_damage()->DamageResource:
+	return current_weapon.damage_resource
+
+## Collects all damage reports into one signal
+func on_damage_report(damage:DamageResource)->void:
+	damage_report.emit(damage)
