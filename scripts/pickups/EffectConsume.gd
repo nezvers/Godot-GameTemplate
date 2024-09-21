39e76e006a224cdb99e69edcc31cf836eae16ed8
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-09-21 22:19:22 +0300

    EffectTransmission, HealthTransmission, item drop

---
+++ scripts/pickups/EffectConsume.gd
@@ -0,0 +1,16 @@
+class_name EffectConsume
+extends Node
+
+@export var effect_transmitter:EffectTransmitter
+@export var animation_player:AnimationPlayer
+@export var fade_out_animation:StringName = "fade_out"
+
+func _ready()->void:
+	effect_transmitter.consumed.connect(on_consumed, CONNECT_ONE_SHOT)
+
+func on_consumed()->void:
+	animation_player.play(fade_out_animation)
+	animation_player.animation_finished.connect(remove, CONNECT_ONE_SHOT)
+
+func remove(_anim:StringName = "")->void:
+	owner.queue_free()
