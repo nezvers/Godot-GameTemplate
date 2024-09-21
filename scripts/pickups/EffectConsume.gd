08bd168368c6129d0b07c5b63f36c6b3b298e8fb
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-09-21 22:45:51 +0300

    Health pickup consume sound

--- scripts/pickups/EffectConsume.gd
+++ scripts/pickups/EffectConsume.gd
@@ -4,11 +4,13 @@ extends Node
 @export var effect_transmitter:EffectTransmitter
 @export var animation_player:AnimationPlayer
 @export var fade_out_animation:StringName = "fade_out"
+@export var sounds_resource:SoundResource
 
 func _ready()->void:
 	effect_transmitter.consumed.connect(on_consumed, CONNECT_ONE_SHOT)
 
 func on_consumed()->void:
+	sounds_resource.play_managed()
 	animation_player.play(fade_out_animation)
 	animation_player.animation_finished.connect(remove, CONNECT_ONE_SHOT)
 
