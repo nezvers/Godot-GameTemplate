4859dea164d1ad38b1f70d00809d4d0e06d85a66
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-09-22 22:27:32 +0300

    cleanup

--- scripts/actor/MoverTopDown2D.gd
+++ scripts/actor/MoverTopDown2D.gd
@@ -12,10 +12,7 @@ extends Node2D
 ## Used for faking angled perspective movement
 @export var axis_multiplier:Vector2 = Vector2(1.0, 1.0)
 
-## Projected maximal velocity
-var target_velocity:Vector2
-## Ammount of velocity acceleration
-var acceleration:float
+
 
 
 ## Way to disable functionality during the gameplay
@@ -27,16 +24,19 @@ func _ready()->void:
 	set_enabled(enabled)
 
 func _physics_process(delta:float)->void:
-	target_velocity = actor_stats_resource.max_speed * input_resource.axis * axis_multiplier
-	var direction:Vector2 = target_velocity - character.velocity 
-	var distance:float = direction.length()
-	acceleration = delta * actor_stats_resource.acceleration
-	var ratio:float = 0
-	if distance > 0.0:
-		ratio = min(acceleration / distance, 1.0)
-	character.velocity += (direction * ratio)
+	var target_velocity:Vector2 = actor_stats_resource.max_speed * input_resource.axis * axis_multiplier
+	character.velocity += get_impulse(character.velocity, target_velocity, actor_stats_resource.acceleration, delta)
 	var _collided:bool = character.move_and_slide()
 
 ## Adds an impulse to velocity, like a kickback
 func add_impulse(impulse:Vector2)->void:
 	character.velocity += impulse
+
+func get_impulse(velocity:Vector2, target_velocity:Vector2, acceleration:float, delta:float)->Vector2:
+	var direction:Vector2 = target_velocity - character.velocity 
+	var distance:float = direction.length()
+	acceleration = delta * acceleration
+	var ratio:float = 0
+	if distance > 0.0:
+		ratio = min(acceleration / distance, 1.0)
+	return (direction * ratio)
