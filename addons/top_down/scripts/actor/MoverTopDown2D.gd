f9581e3f9d11c329d0ada0aa9930954a91586782
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-10-30 09:29:23 +0200

    remove last dependencies on Mover2D

--- addons/top_down/scripts/actor/MoverTopDown2D.gd
+++ addons/top_down/scripts/actor/MoverTopDown2D.gd
@@ -1,5 +1,5 @@
 class_name MoverTopDown2D
-extends Node2D
+extends Node
 
 ## Way to disable functionality at _ready
 @export var enabled:bool = true
--- addons/top_down/scripts/actor/PlayerInput.gd
+++ addons/top_down/scripts/actor/PlayerInput.gd
@@ -2,16 +2,20 @@ class_name PlayerInput
 extends Node
 
 @export var enabled:bool = true
-@export var mover:MoverTopDown2D
+## Used for getting mouse local position
+@export var position_node:Node2D
 ## Offset aim to the body to shoot at shadow collider (feet position)
 @export var aim_offset:Vector2
+@export var resource_node:ResourceNode
 
+var input_resource:InputResource
 
 func set_enabled(value:bool)->void:
 	enabled = value
 	set_physics_process(enabled)
 
 func _ready()->void:
+	input_resource = resource_node.get_resource("input")
 	# Read input before mover
 	process_physics_priority -= 1
 	set_enabled(enabled)
@@ -21,18 +25,18 @@ func _physics_process(_delta:float)->void:
 	var axis:Vector2 = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	if axis.length() > 1.0:
 		axis = axis.normalized()
-	mover.input_resource.set_axis(axis)
+	input_resource.set_axis(axis)
 	
 	# Aiming
-	var aim_direction:Vector2 = mover.get_local_mouse_position() + aim_offset
-	mover.input_resource.set_aim_direction(aim_direction.normalized())
+	var aim_direction:Vector2 = position_node.get_local_mouse_position() + aim_offset
+	input_resource.set_aim_direction(aim_direction.normalized())
 	
 	# Shooting
-	mover.input_resource.set_action(Input.is_action_pressed("shoot"))
+	input_resource.set_action(Input.is_action_pressed("shoot"))
 	
 	var weapon_switch_dir:int = int(Input.is_action_just_released("weapon_up")) - int(Input.is_action_just_released("weapon_down"))
 	if weapon_switch_dir != 0:
-		mover.input_resource.set_switch_weapon(weapon_switch_dir)
+		input_resource.set_switch_weapon(weapon_switch_dir)
 	
 	# Dashing
-	mover.input_resource.set_action_2(Input.is_action_pressed("dash"))
+	input_resource.set_action_2(Input.is_action_pressed("dash"))
