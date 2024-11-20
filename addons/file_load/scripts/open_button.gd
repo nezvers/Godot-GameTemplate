a393a48df7a1a4916b9c6d28baf5c5afba7cb61f
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-11-20 22:28:20 +0200

    file dialogue use example

--- addons/file_load/scripts/open_button.gd
+++ addons/file_load/scripts/open_button.gd
@@ -3,5 +3,12 @@ extends Button
 @export var file_dialog:FileDialogManager
 @export var cache_mode:ResourceLoader.CacheMode
 
+func _ready()->void:
+	file_dialog.selected.connect(on_selected)
+
 func _pressed()->void:
 	file_dialog.desktop_choose_file("open", "open", FileDialog.FileMode.FILE_MODE_OPEN_FILE, [])
+
+func on_selected()->void:
+	# Use selected files or directories
+	print(file_dialog.file_path_list)
