86cef296534bd01f5a227f8f0e040ff49aaa344d
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-12-22 22:56:49 +0200

    cleanup

--- addons/top_down/scripts/weapon_system/projectile/HitLimit.gd
+++ addons/top_down/scripts/weapon_system/projectile/HitLimit.gd
@@ -4,16 +4,15 @@ extends Node
 @export var projectile:Projectile2D
 @export var data_transmitter:DataTransmitter
 
-## Projectile is removed when reaching 0
 ## Starting with negative has infinite count
-@export var hit_limit:int = 1
+@export var target_hit_limit:int = 1
 
 var remaining_hits:int
 
 func _ready()->void:
-	if hit_limit < 1:
+	if target_hit_limit < 1:
 		return
-	remaining_hits = hit_limit
+	remaining_hits = target_hit_limit
 	if !data_transmitter.success.is_connected(on_hit):
 		data_transmitter.success.connect(on_hit)
 
