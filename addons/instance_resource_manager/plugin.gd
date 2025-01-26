118783666f5a28d4762c259415edb83356b3bab0
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2025-01-26 02:27:21 +0200

    manager plugin for instance resources

---
+++ addons/instance_resource_manager/plugin.gd
@@ -0,0 +1,17 @@
+@tool
+extends EditorPlugin
+
+const MANAGER_PANEL = preload("res://addons/instance_resource_manager/scenes/manager_panel.tscn")
+
+var manager_panel:Control
+
+func _enter_tree() -> void:
+	manager_panel = MANAGER_PANEL.instantiate()
+	manager_panel.name = "Instance Resources"
+	
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, manager_panel)
+
+
+func _exit_tree() -> void:
+	remove_control_from_docks(manager_panel)
+	manager_panel.queue_free()
---
+++ addons/instance_resource_manager/scripts/manager_panel.gd
@@ -0,0 +1,134 @@
+@tool extends Node
+
+@export var list_parent:Control
+
+@export var directory_entry:LineEdit
+
+@export var search_entry:LineEdit
+
+var root_directory:String
+
+var search_name:String
+
+var resource_array:Array[InstanceResource]
+
+var resource_dictionary:Dictionary
+
+func _ready()->void:
+	directory_entry.text_submitted.connect(_set_root_directory)
+	print("Manager ready")
+	_set_root_directory(directory_entry.text)
+	
+	search_entry.text_changed.connect(_search_match)
+	_search_match(search_name)
+
+func update_resources()->void:
+	_set_root_directory(directory_entry.text)
+
+func _set_root_directory(value:String)->void:
+	print("New directory: ", value)
+	root_directory = value
+	resource_array.clear()
+	resource_dictionary.clear()
+	
+	# Remove curent list
+	for _child:Node in list_parent.get_children():
+		list_parent.remove_child(_child)
+		_child.queue_free()
+	
+	if root_directory.is_empty():
+		## TODO: Cleanup
+		return
+	
+	if !DirAccess.dir_exists_absolute(root_directory):
+		## TODO: Cleanup
+		return
+	
+	_scan_directory_recursively(root_directory)
+	_search_match(search_name)
+
+func _scan_directory_recursively(path:String)->void:
+	#print("Scanning directory: ", path)
+	var _current_directory: = DirAccess.open(path)
+	if _current_directory == null:
+		## TODO: Cleanup
+		return
+	
+	var _file_list:PackedStringArray = _current_directory.get_files()
+	#print("Files found: ", _file_list)
+	
+	var _last_index:int = _file_list.size() -1
+	for i:int in _file_list.size():
+		var _file_name:String = _file_list[i]
+		if _file_name.get_extension() != "tres":
+			#_file_name.erase(_last_index - i)
+			continue
+		_load_resource(path + _file_name)
+	
+	var _sub_dir_list:PackedStringArray = _current_directory.get_directories()
+	for _dir:String in _sub_dir_list:
+		_scan_directory_recursively(path + _dir + "/")
+
+
+func _load_resource(path:String)->void:
+	var _file_name:String = path.get_file().get_basename()
+	#print("Loading resource: ", _file_name)
+	
+	var _resource:Resource = ResourceLoader.load(path)
+	if _resource == null:
+		return
+	if !(_resource is InstanceResource):
+		return
+	resource_array.append(_resource)
+	
+	resource_dictionary[_file_name] = _resource
+	
+	if !_resource.resource_name.is_empty():
+		resource_dictionary[_resource.resource_name] = _resource
+
+
+func _add_item(_resource:Resource)->void:
+	if _resource == null:
+		return
+	
+	var _editor_resource_picker: = EditorResourcePicker.new()
+	_editor_resource_picker.edited_resource = _resource
+	list_parent.add_child(_editor_resource_picker)
+	_editor_resource_picker.editable = true
+	_editor_resource_picker.base_type = "InstanceResource"
+	#var _label: = Label.new()
+	#_label.text = _resource.resource_path.get_file().get_basename()
+	#list_parent.add_child(_label)
+
+func _search_match(value:String)->void:
+	search_name = value
+	#print("Searching value: ", search_name)
+	
+	# Remove curent list
+	for _child:Node in list_parent.get_children():
+		list_parent.remove_child(_child)
+		_child.queue_free()
+	
+	#Show all
+	if search_name.is_empty():
+		for _resource:Resource in resource_array:
+			_add_item(_resource)
+		return
+	
+	# Fuzzy search
+	var _keys:Array = resource_dictionary.keys()
+	var _filter_callback:Callable = func(key:String)->bool:
+		return key.contains(value)
+	_keys = _keys.filter(_filter_callback)
+	
+	
+	# skipp duplicate entries for resource_name and rsource_path
+	var _unique_array:Array[Resource]
+	for _key:String in _keys:
+		var _resource:Resource = resource_dictionary[_key]
+		if _unique_array.has(_resource):
+			continue
+		_unique_array.append(_resource)
+	
+	for _resource:Resource in _unique_array:
+		_add_item(_resource)
