e3b3c7f8cc1d30ec72c5577ba71ee3c58112161c
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2025-02-13 18:35:47 +0200

    fix typo

--- addons/great_games_library/static/ThreadUtility/ThreadUtility.gd
+++ addons/great_games_library/static/ThreadUtility/ThreadUtility.gd
@@ -4,7 +4,7 @@ extends Node
 ## Load a resource from path using a thread
 static func load_resource(path:String, receive_callback:Callable)->void:
 	# create thread function to load resource and shut down the tread
-	var _tread_load:Callable = func (path:String, callback:Callable, thread:Thread)->PackedScene:
+	var _thread_load:Callable = func (path:String, callback:Callable, thread:Thread)->PackedScene:
 		var _resource:Resource = load(path)
 		callback.call_deferred(_resource)
 		
@@ -14,12 +14,12 @@ static func load_resource(path:String, receive_callback:Callable)->void:
 	
 	# Temporary thread that will shut down itself in the thread function
 	var _thread:Thread = Thread.new()
-	_thread.start(_tread_load.bind(path, receive_callback, _thread))
+	_thread.start(_thread_load.bind(path, receive_callback, _thread))
 
 ## Load a resource from data list {path:String, callback:Callable(resource)}
 static func load_resource_list(data_list:Array[Dictionary], finish_callback:Callable = Callable())->void:
 	# create thread function to load resources and shut down the tread
-	var _tread_load:Callable = func (data_list:Array[Dictionary], finish_callback:Callable, thread:Thread)->PackedScene:
+	var _thread_load:Callable = func (data_list:Array[Dictionary], finish_callback:Callable, thread:Thread)->PackedScene:
 		for _data:Dictionary in data_list:
 			var _resource:Resource = load(_data.path)
 			_data.callback.call_deferred(_resource)
@@ -32,7 +32,7 @@ static func load_resource_list(data_list:Array[Dictionary], finish_callback:Call
 	
 	# Temporary thread that will shut down itself in the thread function
 	var _thread:Thread = Thread.new()
-	_thread.start(_tread_load.bind(data_list, finish_callback, _thread))
+	_thread.start(_thread_load.bind(data_list, finish_callback, _thread))
 
 
 static func callback_list(callback_list:Array[Callable], finish_callback:Callable)->void:
