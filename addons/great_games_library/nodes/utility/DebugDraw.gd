809afd37807df9dbe3aab0c9789889dfff9aa988
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2025-02-19 23:00:08 +0200

    Update DebugDraw.gd

--- addons/great_games_library/nodes/utility/DebugDraw.gd
+++ addons/great_games_library/nodes/utility/DebugDraw.gd
@@ -21,7 +21,7 @@ func erase(callback:Callable)->void:
 
 var list:Array[Callable]
 
-static func debug_line2d(from:Vector2, to:Vector2, color:Color, time:float)->void:
+static func debug_line2d(from:Vector2, to:Vector2, color:Color = Color.RED, time:float = 1.0)->void:
 	if singleton == null:
 		return
 	var _callback:Callable = singleton.draw_line.bind(from, to, color)
@@ -30,10 +30,19 @@ static func debug_line2d(from:Vector2, to:Vector2, color:Color, time:float)->voi
 	_tween.tween_callback(singleton.erase.bind(_callback)).set_delay(time)
 	singleton.queue_redraw()
 
-static func debug_distance2d(from:Vector2, distance:Vector2, color:Color, time:float)->void:
+static func debug_distance2d(from:Vector2, distance:Vector2, color:Color = Color.RED, time:float = 1.0)->void:
 	if singleton == null:
 		return
 	var _callback:Callable = singleton.draw_line.bind(from, from + distance, color)
+	singleton.list.append(_callback)
+	var _tween:Tween = singleton.create_tween()
+	_tween.tween_callback(singleton.erase.bind(_callback)).set_delay(time)
+	singleton.queue_redraw()
+
+static func debug_rectangle(rect:Rect2, filled:bool = true, color:Color = Color.RED, time:float = 1.0)->void:
+	if singleton == null:
+		return
+	var _callback:Callable = singleton.draw_rect.bind(rect, color, filled)
 	singleton.list.append(_callback)
 	var _tween:Tween = singleton.create_tween()
 	_tween.tween_callback(singleton.erase.bind(_callback)).set_delay(time)
